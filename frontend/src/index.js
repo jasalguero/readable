d017fbd043ec01b53359e65ea51bcfe2d89b443c
jasalguero <jasalguero@gmail.com>
2017-10-06 23:08:46 +0530

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { applyMiddleware, createStore, compose } from "redux";
+import reducer from "./reducers";
+import { Provider } from "react-redux";
+import logger from 'redux-logger'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(logger))
+);
+console.log(store.getState());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
