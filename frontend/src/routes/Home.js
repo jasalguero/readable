7e6cb4528f0e04d115cc68d0c90a5839fc25ff1b
jasalguero <jasalguero@gmail.com>
2017-10-08 18:30:18 +0530

    Feat: Added "add post" button in Home route

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,27 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from "react-router-dom";
+import RaisedButton from "material-ui/RaisedButton";
+
 import CategoryList from "../components/CategoryList";
 import PostList from "../components/PostList";
+import AddPostIcon from "react-icons/lib/fa/pencil-square";
 
 class HomeRoute extends Component {
   render() {
     return (
       <div className="home">
         <h1>Home</h1>
-        <ul>
-          <li>
-            should have a control for changing the sort method for the list,
-            including at minimum, order by voteScore and order by timestamp
-          </li>
-          <li>should have a control for adding a new post</li>
-        </ul>
 
         {/* CATEGORIES */}
         <CategoryList categories={this.props.categories} />
 
-         {/* POST LIST */}
-         <PostList posts={this.props.posts} />
+        {/* POST LIST */}
+        <PostList posts={this.props.posts} />
+
+        {/* ADD POST*/}
+        <RaisedButton
+          containerElement={<Link to="posts/new" />}
+          label="Create Post"
+          primary={true}
+          icon={<AddPostIcon />}
+        />
       </div>
     );
   }
